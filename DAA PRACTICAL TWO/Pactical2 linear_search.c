d7b06c648c4f6ad431c90693e81a8341deb9d62c
JULIUS STEPHEN OLIVER LUKADI <juliusstephen202@gmail.com>
2026-08-15 09:20:38 +0530

    Add files via upload

---
+++ DAA PRACTICAL TWO/Pactical2 linear_search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, target, found = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            printf("Element %d found at position %d.\n", target, i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+        printf("Element not found.\n");
+
+    return 0;
+}
